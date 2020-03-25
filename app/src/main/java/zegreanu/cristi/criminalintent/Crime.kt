94881f962ad32089ceb3c23000b727416334ddd3
czegreanu <cristi.zegreanu@gmail.com>
2020-03-25 12:18:02 +0200

    Chapter 8 challenges
    Different UI for requirePolice rows

--- app/src/main/java/zegreanu/cristi/criminalintent/Crime.kt
+++ app/src/main/java/zegreanu/cristi/criminalintent/Crime.kt
@@ -8,9 +8,11 @@ class Crime constructor() {
     var date: Date = Date()
     var title: String = ""
     var solved: Boolean = false
+    var requirePolice: Boolean = false
 
-    constructor(title: String, solved: Boolean) : this() {
+    constructor(title: String, solved: Boolean, requirePolice: Boolean) : this() {
         this.title = title
         this.solved = solved
+        this.requirePolice = requirePolice
     }
 }
