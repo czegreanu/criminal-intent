f8b1b24f40d3b8b98bc579960209eb50a7faf21e
czegreanu <cristi.zegreanu@gmail.com>
2020-03-25 17:11:23 +0200

    Chapter 10 implementation
    Link the pages (list page with details page)

--- app/src/main/java/zegreanu/cristi/criminalintent/CrimeActivity.kt
+++ app/src/main/java/zegreanu/cristi/criminalintent/CrimeActivity.kt
@@ -1,9 +1,23 @@
 package zegreanu.cristi.criminalintent
 
+import android.content.Context
+import android.content.Intent
 import androidx.fragment.app.Fragment
+import java.util.UUID
 
 class CrimeActivity : SingleFragmentActivity() {
     override fun createFragment(): Fragment {
-        return CrimeFragment()
+        return CrimeFragment.newInstance(intent.getSerializableExtra(EXTRA_CRIME_ID) as UUID)
+    }
+
+    companion object {
+        const val EXTRA_CRIME_ID = "zegreanu.cristi.criminalintent.crime_id"
+
+        fun newIntent(packageContext: Context, crimeId: UUID): Intent {
+            return Intent(packageContext, CrimeActivity::class.java).putExtra(
+                EXTRA_CRIME_ID,
+                crimeId
+            )
+        }
     }
 }
